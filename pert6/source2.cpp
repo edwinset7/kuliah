fe21c153fe5428eb310e33809578de203998ecad
root <root@EwottPC.localdomain>
2021-11-02 03:16:03 +0700

    pertemua_ke6

--- pert6/source2.cpp
+++ pert6/source2.cpp
@@ -5,7 +5,7 @@ using namespace std;
 long factorial (long a);
 int main (){
     int n;
-     long hasil;
+    long hasil;
     cout<<"Menghitung N Faktorial (N!) \n";
     cout<<"Masukan N : ";cin>>n;
     hasil = factorial(n);
@@ -16,8 +16,8 @@ int main (){
 
 long factorial (long a)
 { 
-     long i, hasil1=1;
+    long i, hasil1=1;
     for (i=1; i<=a; i++)
-     hasil1 = hasil1*i;
+    hasil1 = hasil1*i;
     return (hasil1);
 }
